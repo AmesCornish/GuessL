8d4cb03a5dbc2c594cbf03fde055d36324417137
Ames <git@sys.cornishes.net>
2022-01-11 18:13:24 -0800

    Uses clue that chars are elsewhere

--- guess.py
+++ guess.py
@@ -16,48 +16,116 @@ def joinSetAndList(s: set[str], l: list[str]) -> str:
 def setToStr(s: set[str]):
     return ''.join(list(s))
 
-class Clues:
-    clues = {}
-    cacheHits = 0
-    cacheMisses = 0
+def inCharsToSets(s: str):
+    return [set(ins) for ins in s.split('/')]
+
+class CorrectClue:
+    # word has to match pattern of letters with known locations
+
+    def __init__(self, chars: list[str]):
+        self.chars = chars
+
+    def match(self, word: str) -> bool:
+        for (w, s) in zip(word, self.chars):
+            if s != '.' and w != s: return False
+        return True
+
+    def combine(self, clue):
+        return CorrectClue([s if s != '.' else c for (s,c) in zip(self.chars, clue.chars)])
+
+    def __str__(self) -> str:
+        return ''.join(self.chars)
 
     @staticmethod
-    def makeClues(pattern: str, inChars: str, outChars: str):
+    def parse(s: str):
+        return CorrectClue(list(s))
+
+    @staticmethod
+    def fromGuess(guess: str, word: str):
+        return CorrectClue([(g if g == w else '.') for (w,g) in zip(word, guess)])
+
+class NotFoundClue:
+    def __init__(self, chars: list[str]):
+        self.chars = chars
+
+    def match(self, word: str) -> bool:
+        for c in self.chars:
+            if c in word: return False
+        return True
+
+    def combine(self, clue):
+        return NotFoundClue(self.chars + clue.chars)
+
+    def __str__(self) -> str:
+        return ''.join(self.chars)
+
+    @staticmethod
+    def parse(s: str):
+        return NotFoundClue(list(s))
+
+    @staticmethod
+    def fromGuess(guess: str, word: str):
+        return NotFoundClue([g for g in guess if g not in word])
+
+
+class ElsewhereClue:
+    # If char is *elsewhere* in word, it shouldn't be in this position
+
+    def __init__(self, chars: list[set[str]]):
+        self.chars = chars
+
+    def match(self, word: str) -> bool:
+        for (ins, wordChar) in zip(self.chars, word):
+            if wordChar in ins: return False
+            # But it must be in the word somewhere
+            for inChar in ins: 
+                if inChar not in word: return False
+        return True
+
+    def combine(self, clue):
+        return ElsewhereClue([s.union(c) for (s,c) in zip(self.chars, clue.chars)])
+
+    def __str__(self) -> str:
+        return '/'.join([''.join(c) for c in self.chars])
+
+    @staticmethod
+    def parse(s: str):
+        return ElsewhereClue([set(list(chars)) for chars in s.split('/')])
+
+    @staticmethod
+    def fromGuess(guess: str, word: str):
+        return ElsewhereClue([(set(g) if g in word and g != w else set())for (g,w) in zip(guess, word)])
+
+class Clues:
+    clues = {}
+
+    @staticmethod
+    def makeClues(correct: CorrectClue, elsewhere: ElsewhereClue, notFound: NotFoundClue):
         # Try to use an existing Clues object, because
-        # it caches the elimated word list
-        clues = Clues(pattern, inChars, outChars)
+        # it caches the eliminated word list
+        clues = Clues(correct, elsewhere, notFound)
         existingClues = Clues.clues.get(clues)
         if existingClues is not None:
-            Clues.cacheHits += 1
             return existingClues
-        Clues.cacheMisses += 1
         Clues.clues[clues] = clues
-        # print(f"New Clues: {clues}")
         return clues
 
-    def __init__(self, pattern: str, inChars: str, outChars: str):
-        self.pattern = pattern
-        self.re = re.compile(pattern)
-
-        inSet = set(pattern)
-        if '.' in pattern:
-            inSet.remove('.')
-        self.inChars = frozenset(inSet.union(set(inChars)))
-
-        self.outChars = frozenset(outChars)
-        self.outPattern = re.compile('|'.join(list(self.outChars))) if len(self.outChars) > 0 else None
+    def __init__(self, correct: CorrectClue, elsewhere: ElsewhereClue, notFound: NotFoundClue):
+        self.correct = correct
+        self.elsewhere = elsewhere
+        self.notFound = notFound
 
         self._words = None
         self._count = None
     
     def __str__(self) -> str:
-        return f"{self.pattern}/{setToStr(self.inChars)}/{setToStr(self.outChars)}"
+        return ' '.join(f"'{s}'" for s in [self.correct, self.elsewhere, self.notFound])
 
     def testWord(self, word):
-        if not self.re.match(word): return False
-        for ch in self.inChars:
-            if ch not in word: return False
-        if self.outPattern is not None and self.outPattern.search(word): return False
+        if not self.correct.match(word): return False
+        if not self.notFound.match(word): return False
+        if not self.elsewhere.match(word): return False
+
         return True
 
     def getWords(self, words):
@@ -79,43 +147,64 @@ class Clues:
         words = self.getWords(wordList)
         lenWords = len(words)
 
+        # print(f"Guessing {guess}")
+
         eliminations = lenWords*1.0
         for word in words:
-            clues = self.update(guess, word)
-            eliminations -= clues.countWords(words)/lenWords
+            if word == guess: continue
+            newClues = Clues.fromGuess(guess, word)
+            clues = self.combine(newClues)
+            count = clues.countWords(words)
+            # print(f"If the word is {word}, we get {newClues} with {count} matches")
+            eliminations -= count/lenWords
         return eliminations/lenWords
 
-    def update(self, guess: str, actual: str):
-        outChars = [c for c in guess if c not in actual]
-        inChars = [c for c in guess if c in actual]
+    def combine(self, clues):
+        return Clues.makeClues(
+            self.correct.combine(clues.correct), 
+            self.elsewhere.combine(clues.elsewhere), 
+            self.notFound.combine(clues.notFound),
+        ) 
 
-        pattern = list(self.pattern)
-        for i in range(len(actual)):
-            if guess[i] == actual[i]: pattern[i] = actual[i]
-
-        return Clues.makeClues(''.join(pattern), joinSetAndList(self.inChars, inChars), joinSetAndList(self.outChars, outChars))
+    @staticmethod
+    def fromGuess(guess: str, actual: str):
+        return Clues.makeClues(
+            CorrectClue.fromGuess(guess, actual), 
+            ElsewhereClue.fromGuess(guess, actual), 
+            NotFoundClue.fromGuess(guess, actual),
+        ) 
 
     def __hash__(self):
         return hash(str(self))
     
     def __eq__(self, o: object) -> bool:
-        if (not self.pattern == o.pattern): return False
-        if (not self.inChars == o.inChars): return False
-        if (not self.outChars == o.outChars): return False
-        return True
+        return str(self) == str(o)
     
 def main(argv=sys.argv):
-    print ("Wordl Guesser")
     ### DEBUG
-    # argv = [ '.....', '', '' ]
-    # argv = [ '..a..', 'abr', 'zteg' ]
-    words = readWords()
-    clues = Clues.makeClues(argv[1], argv[2], argv[3])
+    # argv = [ None, '.....', '////', '' ]
+    # argv = [ None, '.....', 'd/o/u/l/', 'taresiy' ]
+    # argv = [ None, '...e.', '//r//', 'tas' ]
+    # argv = [ None, '.o.e.', 'd//r//', 'tasily' ]  ## Suspicious answer
+    # argv = [ None, '.o.ed', 'd//r//', 'tasilybp' ]
+    ### DEBUG
 
-    possibilities = len(clues.getWords(words))
+    allWords = readWords()
+    clues = Clues.makeClues(
+        CorrectClue.parse(argv[1]),
+        ElsewhereClue.parse(argv[2]),
+        NotFoundClue.parse(argv[3]),
+    )
+
+    possibilities = len(clues.getWords(allWords))
+
+    print ("Wordl Guesser")
+    print (f"Clues: {clues}")
     print(f"There are {possibilities} possibilities left")
+
+    if possibilities <= 10:
+        for word in clues.getWords(allWords): print(word)
     if possibilities <= 2:
-        for word in clues.getWords(words): print(word)
         return 0
 
     bestGuess = ""
@@ -124,8 +213,10 @@ def main(argv=sys.argv):
     currentTime = startTime
     count = 0
 
-    for guess in words:
-        value = clues.valueOfGuess(guess, clues.getWords(words))
+    words = clues.getWords(allWords)
+    for guess in allWords:
+    # for guess in ['biped', 'aloes', 'tares', 'doily']:
+        value = clues.valueOfGuess(guess, words)
 
         count += 1
         currentTime = time.process_time()
@@ -133,9 +224,9 @@ def main(argv=sys.argv):
         if theTimeLimit is not None and totalTime > theTimeLimit: return 1
 
         if value < bestValue: continue
-        if value == bestValue and guess not in clues.getWords(words): continue
+        if value == bestValue and guess not in words: continue
 
-        print(f"...Found {guess}:{value:.1%} ({totalTime:.1f} seconds)")
+        print(f"...Found {guess}:{value:.1%} ({totalTime:.0f}/{totalTime*len(allWords)/count:.0f} seconds)")
         bestGuess = guess
         bestValue = value
 
